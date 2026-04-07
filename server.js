bb13119a9bd25158a82d89ccac6ddbe63bd3deb6
Imane <imanebg1234@gmail.com>
2026-04-07 14:34:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -22,28 +22,16 @@ function saveCounter(count) {
 }
 
 let visitCount = readCounter();
+const serverName = os.hostname();
 
 app.get('/', (req, res) => {
-
   visitCount++;
   saveCounter(visitCount);
 
-  const hostname = os.hostname();
-  const serverIP = req.socket.localAddress;
-  const clientIP = req.socket.remoteAddress;
-  const clientPort = req.socket.remotePort;
-
   res.send(`
-  <h1>Visit Counter</h1>
-  <p>Visits: ${visitCount}</p>
-
-  <h2>Server Info</h2>
-  <p>Hostname: ${hostname}</p>
-  <p>Port: ${port}</p>
-  <p>Server IP: ${serverIP}</p>
-
-  <h2>Client Info</h2>
-  <p>IP: ${clientIP}:${clientPort}</p>
+    <h1>Visit Counter</h1>
+    <p>Visits: ${visitCount}</p>
+    <p>Server: ${serverName}</p>
   `);
 });
 
