93ab7c764438b2fdfaf942852b9fcf0e73614db3
Imane <imanebg1234@gmail.com>
2026-04-07 03:21:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ res.send(`
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => {
-console.log("Server running on port " + port);
+app.listen(port, "0.0.0.0", () => {
+  console.log("Server running on port " + port);
 });
