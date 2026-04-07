238e39fcc1cddb9a004abddf097c510c0b019de6
Imane <imanebg1234@gmail.com>
2026-04-07 03:26:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
 
+let counter = 0;
+
 app.use(express.static(__dirname));
 
+app.get("/counter", (req, res) => {
+counter++;
+res.send(counter.toString());
+});
+
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-  console.log("Server running");
+console.log("Server running");
 });
